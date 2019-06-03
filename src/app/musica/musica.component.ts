4a4bebe1492335ca4695f84e6f4ca815b2a571ee
Jackson Gomes <jgomes@ceulp.edu.br>
2019-06-03 01:41:22 -0300

    feat(componentes): melhora o componente Musica para usar os novos serviços

--- src/app/musica/musica.component.ts
+++ src/app/musica/musica.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { DiscoService } from '../disco.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { MusicasService } from '../musicas.service';
+import { Musica } from '../models/musica';
+import { GenerosService } from '../generos.service';
+import { forkJoin, Observable, of, from, zip } from 'rxjs';
+import { map, tap, switchMap, single, first, mergeAll, concatAll, zipAll, delay, share } from 'rxjs/operators';
+import { ArtistasService } from '../artistas.service';
 
 @Component({
   selector: 'app-musica',
@@ -8,35 +13,45 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./musica.component.css']
 })
 export class MusicaComponent implements OnInit {
-  musica = null;
+  musica: Musica = null;
   relacionadas = null;
 
-  constructor(private disco: DiscoService, private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private musicas$: MusicasService) { }
 
   ngOnInit() {
-    // Perceba a utilização de outra abordagem para obter o valor do parâmetro de rota:
-    // utilizar o atributo paramMap e o método subscribe()
-    // Isso é necessário porque, ao apresentar a lista das músicas relacionadas
-    // o link para cada música não funciona porque a URL é a mesma do caminho de rota
-    // da música atual, apenas o valor do parâmetro. 
-    // A abordagem de usar o atributo snapshot
-    // só funciona se a URL combinar com um caminho de rota diferente.
-    this.route.paramMap.subscribe((params: ParamMap) => {
-      const id = params.get('id');
-      this.musica = this.disco.encontrarMusica(parseInt(id));
-      this.disco.preencherObjetoMusica(this.musica);
-      this.relacionadas = this.disco.listaDeMusicasRelacionadas(this.musica);
-    });
+    this.route.paramMap
+      .subscribe((params: ParamMap) => {
+        this.relacionadas = null;
+        const id = params.get('id');
+        this.musicas$.encontrar(parseInt(id))
+          .subscribe(musica => this.musica = musica);
+      });
+
+    // this.route.paramMap.pipe(
+    //   map(params => params.get('id')),
+    //   map((id: string) => parseInt(id)),
+    //   map(id => this.musicas$.encontrar(id)),
+    //   mergeAll(),
+    // ).subscribe(
+    //   musica => this.musica = musica
+    // );
   }
 
   gostar() {
-    this.disco.gostar(this.musica);
-    this.relacionadas = this.disco.listaDeMusicasRelacionadas(this.musica);
+    this.musica.gostar = 1;
+    this.musica.naoGostar = 0;
+    this.atualizarListaDeRelacionadas();
   }
 
   naoGostar() {
-    this.disco.naoGostar(this.musica);
-    this.relacionadas = this.disco.listaDeMusicasRelacionadas(this.musica);
+    this.musica.gostar = 0;
+    this.musica.naoGostar = 1;
+    this.atualizarListaDeRelacionadas();
+  }
+
+  atualizarListaDeRelacionadas() {
+    this.musicas$.listaRelacionadas(this.musica)
+      .subscribe(musicas => this.relacionadas = musicas);
   }
 
 }
